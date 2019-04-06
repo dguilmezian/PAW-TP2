6ab4b3b3f291095e24e2b97247116b8fb13cb921
Daniel Guilmezian <dguilmezian@gmail.com>
2019-04-06 16:05:48 -0300

    Reestructurado el TP2.

--- README.md
+++ README.md
@@ -12,3 +12,18 @@
 <hr>
 <h1>Para hacer andar el Framework</h1>   
 <p>Abrir una consola de comandos en la carpeta /tp2 y escribir el comando php -S localhost:8000</p>
+<hr>
+<h2>Estructura</h2>
+<p>-controllers
+---ej1
+------ej1.php
+---ej2
+------ej2.php
+---ej3 
+-views
+---ej1
+------ej1.view.php
+---ej2
+------ej2.view.php
+---ej3
+</p>
--- tp2/routes.php
+++ tp2/routes.php
@@ -2,10 +2,12 @@
 
 $app->router->define([
     '' => 'controllers/index.php',
-    'about' => 'controllers/about.php',
-    'about/us' => 'controllers/about-us.php',
-    'contact' => 'controllers/contact.php',
-    'tp2ej1'=>'controllers/tp2ej1.php',
-    'tp2ej3'=>'controllers/tp2ej3.php',
-    'tp2ej3_results'=>'views/tp2ej3_results.php',
+    'tp2ej1'=>'controllers/tp2ej1/tp2ej1.php',
+    'tp2ej3'=>'controllers/tp2ej3/tp2ej3.php',
+    'tp2ej3_results'=>'views/tp2ej3/tp2ej3_results.php',
+    // descomentar
+    // 'tp2ej4'=>'controllers/tp2ej4/tp2ej4.php',
+    // 'tp2ej5'=>'controllers/tp2ej5/tp2ej5.php',
+    // 'tp2ej6'=>'controllers/tp2ej6/tp2ej6.php',
+    // 'tp2ej7'=>'controllers/tp2ej7/tp2ej7.php',
 ]);
